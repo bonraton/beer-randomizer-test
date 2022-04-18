4cf1141c5a63b8a73e700175a335fd77ffc5dc4b
bonraton <bonraton@yandex.ru>
2022-04-18 21:36:51 +0300

    feat: joke generator

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,7 +1,14 @@
-import { pintVolume, averageVidmarkCoef } from './constant';
+import {
+  pintVolume,
+  averageVidmarkCoef,
+  jokes,
+  bacValues,
+} from './constant';
 
 export function calculateUserAge(birthDate) {
-  return `${new Date().getFullYear() - birthDate.slice(0, 4)} years`;
+  return `${
+    new Date().getFullYear() - birthDate.slice(0, 4)
+  } years`;
 }
 
 // Delete magic values;
@@ -10,4 +17,21 @@ export function calculateBac(alc, weight, pint) {
   const absoluteAlc = alcohol * pintVolume * pint * 10;
   const bac = absoluteAlc / (weight * averageVidmarkCoef);
   return bac.toFixed(2);
+}
+
+export function generateJoke(bac) {
+  switch (true) {
+    case bac < bacValues.low:
+      return jokes.low;
+    case bac < bacValues.medium && bac > bacValues.low:
+      return jokes.medium;
+    case bac < bacValues.strong && bac > bacValues.medium:
+      return jokes.strong;
+    case bac < bacValues.extremal && bac > bacValues.strong:
+      return jokes.extremal;
+    case bac >= bacValues.extremal:
+      return jokes.lethal;
+    default:
+      return '';
+  }
 }
