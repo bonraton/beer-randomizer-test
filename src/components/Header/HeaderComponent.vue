36897a0307195da3d3e4acc463d1cf7cc24bb4ba
bonraton <bonraton@yandex.ru>
2022-04-18 12:35:07 +0300

    feat: Header styles

--- src/components/Header/HeaderComponent.vue
+++ src/components/Header/HeaderComponent.vue
@@ -1,6 +1,14 @@
 <template>
   <header class="header">
-    <h1>Header</h1>
+    <h2 class="header__title">Beer randomizer</h2>
+    <div class="profile">
+      <ul class="profile__info">
+        <li v-for="(item, index) in profile" :key="index" class="profile__item">
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+    <img src="../../images/default_avatar.png" class="profile__image" alt="avatar" />
   </header>
 </template>
 
@@ -9,5 +17,14 @@ import './HeaderComponent.css';
 
 export default {
   name: 'HeaderComponent',
+  data() {
+    return {
+      profile: {
+        name: 'Oleg',
+        age: '29',
+        position: 'Beer destroyer',
+      },
+    };
+  },
 };
 </script>
