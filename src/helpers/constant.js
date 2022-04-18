21aeb7c8d013454daf0cf6e674ad668f54691bf8
bonraton <bonraton@yandex.ru>
2022-04-18 21:38:05 +0300

    feat: jokes and bacValues constants

--- src/helpers/constant.js
+++ src/helpers/constant.js
@@ -3,3 +3,18 @@ export const PROFILE_BASE_URL = 'https://random-data-api.com/api/users/random_us
 
 export const pintVolume = 0.55;
 export const averageVidmarkCoef = 0.65;
+
+export const jokes = {
+  low: 'Keep on party',
+  medium: "You'd better stop",
+  strong: 'Probably you need a doctor',
+  extremal: 'You need a doctor, urgently!',
+  lethal: 'You must be dead!',
+};
+
+export const bacValues = {
+  low: 1,
+  medium: 2,
+  strong: 3,
+  extremal: 4,
+};
