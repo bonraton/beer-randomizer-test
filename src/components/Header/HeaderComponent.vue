b9c17ed2ded4b553d38810396d9bfd9946d652cc
bonraton <bonraton@yandex.ru>
2022-04-19 15:36:25 +0300

    feat: interval request avatar image

--- src/components/Header/HeaderComponent.vue
+++ src/components/Header/HeaderComponent.vue
@@ -12,7 +12,7 @@
           {{ item }}
         </li>
       </ul>
-      <img :src="profile.avatar" class="profile__image" alt="avatar" />
+      <img :src="this.profile.avatar" @error="onAvatarError" class="profile__image" alt="avatar" />
     </div>
   </header>
 </template>
@@ -20,6 +20,7 @@
 <script>
 import './HeaderComponent.css';
 import getRandomProfile from '../../utils/profileApi';
+import defaultImage from '../../images/default_avatar.png';
 
 export default {
   name: 'HeaderComponent',
@@ -28,7 +29,21 @@ export default {
       profile: {},
     };
   },
-  async mounted() {
+  methods: {
+    onAvatarError(e) {
+      let i = 0;
+      if (i < 4) {
+        setTimeout(() => {
+          e.target.src = this.profile.avatar;
+          i += console.log(i);
+        }, 3000);
+      } else {
+        e.target.src = defaultImage;
+        clearInterval();
+      }
+    },
+  },
+  async created() {
     this.profile = await getRandomProfile();
   },
 };
