543ca61e3625063872411aab5b7ea4d2a36bad82
bonraton <bonraton@yandex.ru>
2022-04-20 17:01:30 +0300

    feat: Preloader

---
+++ src/components/Preloader/PreloaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <span v-show="isVisible" class="preloader"></span>
+</template>
+
+<script>
+import './PreloaderComponent.css';
+
+export default {
+  name: 'PreloaderComponent',
+  props: {
+    isVisible: {
+      type: Boolean,
+      required: true,
+    },
+  },
+};
+</script>
